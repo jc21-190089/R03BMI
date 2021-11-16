986b57c236042e09331c594fee65c56473f60b35
成喜 外塚 <190089@jc-21.jp>
2021-11-16 12:24:55 +0900

    身長と体重の入力、BMIの計算の完了

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -13,12 +13,36 @@ namespace R03BMI
         public MainPage()
         {
             InitializeComponent();
-            creater.Text = "JK3A00 吉澤　毅";
+            creater.Text = "JK3A28 外塚 成喜";
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            var height = heighit.Text;
+            var weight = weighit.Text;
+            try
+            {
+                double h = double.Parse(height);
+                int w = int.Parse(weight);
+                if (h > 10)
+                {
+                    h = h / 100;
+                }
+                double bmi = w / (h * h);
+                double bmi2 = Math.Round(bmi, 2);
+
+
+
+                result.Text = "身長" + h + "m体重" + w + "kgの人のBMIは" + bmi2;
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+                result.Text = "!!!";
+            }
 
         }
     }
 }
+
