92854fd62feaf02a8cd1cc0fb0befd850fafcca6
成喜 外塚 <190089@jc-21.jp>
2021-11-19 11:30:25 +0900

    体重の条件を変更(10000→1000)

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -29,7 +29,7 @@ namespace R03BMI
                 {
                     h = h / 100;
                 }
-                if(w>=10000)
+                if(w>=1000)
                 {
                     w = w / 1000;
                 }
